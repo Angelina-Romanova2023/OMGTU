cffea324ef9067a2b96c9de11cc373ad7df31144
Angelina-Romanova2023 <145256051+Angelina-Romanova2023@users.noreply.github.com>
2024-01-13 20:13:03 +0300

    Add files via upload

---
+++ Алгоритмизация и программирование/заправки 25.10.cs
@@ -0,0 +1,33 @@
+/* 25.10 заправки
+Количество городов = k 
+a(i)-расстояние м/ж городами
+p-расстояние ближе которого нельзя располагать заправку к городу
+Определить: положение заправки так, чтобы суммарное расстояние от каждого города до заправки было мин*/
+using System;
+class HelloWorld {
+  static void Main() {
+      Console.WriteLine("k=");
+      int k = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("p=");
+      int p = Convert.ToInt32(Console.ReadLine());
+      int [] a = new int[k];
+      a[0]=0;
+      for (int i=1; i<k;i++)
+      {
+          Console.WriteLine($"a({i})=__>={2*p}");
+          a[i] = Convert.ToInt32(Console.ReadLine());
+          a[i]=a[i]+a[i-1];
+      }
+      if ((k-1)%2==0)
+      {
+          int b1=a[((k-1)/2)]+p;
+          int b2=a[((k-1)/2)]-p;
+          Console.WriteLine($"{b2} или {b1}");
+      }
+      else
+      {
+          int b=a[((k+1)/2)]-p;
+          Console.WriteLine(b);
+      }
+  }
+}
